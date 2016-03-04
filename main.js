b67a568957dd907b4c2d4a6620feecfa0f0e7290
hankgielarowski <w_gielarowski@yahoo.com>
2016-03-04 13:37:24 -0500

    js junk

---
+++ main.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  aleYeah.init();
+})
+
+var aleYeah = {
+  url: {
+
+  }
+},
+
+init:function () {
+  aleYeah.events(),
+  aleYeah.styling(),
+},
+
+events: function () {
+
+},
+
+styling: function () {
+
+},
+
+getBeerName: function () {
+
+},
+
+getBeerType: function () {
+
+},
+
+getBeerAlcoholContent:function(){
+
+},
+
+getBeerReviews: function(){
+
+},
+
+addBeertoDom: function(){
+
+},
+
+addBeerReviewtoDom: function(){
+
+},
