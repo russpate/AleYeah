bd0be03b5f1fc0e53bda9fe28f98fad0e11f381f
drewmunnerlyn <drewmunnerlyn@gmail.com>
2016-03-05 17:47:48 -0500

    updated jar file added a selectUsers method to prove its the javascript thats not working

--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -13,4 +13,29 @@ public class User {
         this.name = name;
         this.password = password;
     }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
